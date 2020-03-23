f377059559dfee70bb89cb09d4178c2ae96771d7
Florian Bouron <bouron.florian@yahoo.fr>
2020-03-23 19:53:09 +0100

    Add Authentication

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,7 +10,9 @@ import {
   UsePipes,
   ValidationPipe,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
@@ -19,6 +21,7 @@ import { GetTaskFilterDto } from './dto/get-task.dto';
 import { TaskStatusValidationPipe } from './pipes/task-satus-validation.pipe';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
